a952ac6a67382a951f9b702d16f250f3ab7654ed
ines-lorquet <ines.lorquet@laplateforme.io>
2024-04-02 14:42:11 +0200

    [Créer méthode tri_selection]

--- sorting.py
+++ sorting.py
@@ -4,3 +4,12 @@ from random import randint
 class Sorting:
     def __init__(self):
         pass
+
+    def tri_selection(self,tab):
+        for i in range(len(tab)-1):
+            i_min = i
+            for j in range(i+1, len(tab)):
+                if tab[j]<tab[i_min]:
+                    i_min=j
+            tab[i],tab[i_min]=tab[i_min],tab[i]
+        return tab
