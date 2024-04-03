b7af219cd78f63440464801acae27e5220742c91
ines-lorquet <ines.lorquet@laplateforme.io>
2024-04-03 09:11:05 +0200

    [Créer class tri_insertion]

--- sorting.py
+++ sorting.py
@@ -4,3 +4,14 @@ from random import randint
 class Sorting:
     def __init__(self):
         pass
+
+    def tri_insertion(self,list):
+        for i in range(1, len(list)):
+            val = list[i]
+            j = i-1
+            while j>=0 and list[j]>val:
+                list[j+1] = list[j]
+                j -= 1
+            list[j+1] = val
+        return list
+
