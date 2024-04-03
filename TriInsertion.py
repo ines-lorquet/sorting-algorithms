fd967f873255ded74be9f585b1034ec7e9254d49
bruno-coulet <bruno.coulet@laplateforme.io>
2024-04-03 17:08:12 +0200

    [Ajout de TriSelection.py]

---
+++ TriInsertion.py
@@ -0,0 +1,17 @@
+class TriInsertion():
+    def __init__(self, arr):
+        self.arr = arr
+
+    def insertion_sort(self):
+        for i in range(1,len(self.arr)):
+            current = self.arr[i]
+            j = i
+            #décalage des éléments du self.arr }
+            while j>0 and self.arr[j-1]>current:
+                self.arr[j]=self.arr[j-1]
+                j = j-1
+            #on insère l'élément à sa place
+            self.arr[j]=current
+
+        # Return the sorted array
+        return self.arr
